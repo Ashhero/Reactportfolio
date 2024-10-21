78ef3fad45f1f6db541141eb487baa25ff32439c
Ashhero <akashdevadiga919@gmail.com>
2024-10-21 23:00:31 +0530

    Updated the file with relevant changes

--- src/services.js
+++ src/services.js
@@ -2,24 +2,24 @@ import React  from "react";
 
 export default  function Services (){
    return( <div>
-    <div id="services">
+    <div id="services" style={{marginTop:'50px'}}>
         <div className="container">
             <h1 className="subtitle">Services</h1>
             <div className="services-list">
                 <div>
                     <i className="fa-solid fa-code"></i>
                     <h2>FrontEnd Developer</h2>
-                    <p>Welcome to my  portfolio! I'm a passionate web developer with a strong background in creating dynamic and visually appealing websites. As a frontend developer with expertise in React, Tailwind CSS, and other modern technologies, you are skilled at building dynamic and responsive user interfaces. React allows you to create efficient, component-based UIs, ensuring seamless user experiences through reusable elements and efficient state management. With Tailwind CSS, you can quickly style applications using utility-first classes, enabling rapid design and customization.</p>
+                    <p>Welcome to my  portfolio! I'm a passionate web developer with a strong background in creating dynamic and visually appealing websites. As a frontend developer with expertise in React, Tailwind CSS, and other modern technologies, you are skilled at building dynamic and responsive user interfaces. React allows you to create efficient, component-based UIs, ensuring seamless user experiences through reusable elements and efficient state management.</p>
                 </div>
                 <div>
                     <i className="fa-brands fa-dev"></i>
                     <h2>Backend Developer</h2>
-                    <p>As a backend developer with experience in Express.js, Node.js, and MongoDB, you possess the essential skills to build robust and scalable server-side applications. Express.js, a flexible web application framework for Node.js, enables you to design APIs and handle HTTP requests efficiently. Using Node.js, you manage server-side logic with asynchronous, event-driven architecture, ensuring high performance and scalability for real-time applications. With MongoDB, a NoSQL database, you handle large volumes of data, enabling dynamic schema designs and easy integration with the server.</p>
+                    <p>As a backend developer with experience in Express.js, Node.js, and MongoDB, you possess the essential skills to build robust and scalable server-side applications. Express.js, a flexible web application framework for Node.js, enables you to design APIs and handle HTTP requests efficiently. Using Node.js, you manage server-side logic with asynchronous, event-driven architecture, ensuring high performance and scalability for real-time applications. With MongoDB, a NoSQL database, you handle large volumes of data, enabling dynamic schema designs.</p>
                     {/* <a href="#">Learn more</a> */}
                 </div>
                 <div>
                     <i className="fa-solid fa-mobile"></i>
-                    <h2>React Native Developer (App Developer)</h2>
+                    <h2>React Native Developer </h2>
                     <p>As a React Native app developer, with expertise in React, Tailwind CSS, and other relevant technologies, I have well-equipped to build cross-platform mobile applications with a seamless and native-like user experience. React Native allows you to use a single codebase to develop apps for both iOS and Android, while maintaining excellent performance and smooth navigation. By leveraging Tailwind CSS (or similar libraries for React Native) for styling, you can create clean, responsive layouts that enhance user experience across various devices.</p>
                     {/* <a href="#">Learn more</a> */}
                 </div>
--- src/work.js
+++ src/work.js
@@ -14,7 +14,7 @@ return(
                     <img src={logo2} alt="this is an work2"/>
                     <div className="layer">
                         <h2 style={{marginBottom:'20px'}}>Mern Notes App </h2>
-                        <p >The MERN stack notes app is a full-stack web application that allows users to create, view, update, and delete notes efficiently. Built using MongoDB for the database, Express.js for the backend server, React for the frontend interface, and Node.js for server-side execution, the app offers a seamless user experience for managing notes. It features user authentication, CRUD functionality, and a responsive UI, making it an ideal tool for organizing personal or professional notes in a structured way.</p><br/>
+                        <p >The MERN stack notes app is a full-stack web application that allows users to create, view, update, and delete notes efficiently. Built using MongoDB for the database, Express.js for the backend server, React for the frontend interface, and Node.js for server-side execution, the app offers a seamless user experience for managing notes.</p><br/>
                         <h3 style={{marginTop:"20px"}}>Click Here</h3>
                     </div>
                     </a>
@@ -23,9 +23,9 @@ return(
                     <a href="https://github.com/Ashhero/Gourmet-Genius/tree/main" target="_blank" rel="noopener noreferrer">
                     <img src={logo3} alt="this is an work3"/>
                     <div className="layer">
-                        <h2 style={{marginBottom:'20px'}}>Gourmet Genius(Online Catering System)</h2>
+                        <h2 style={{marginBottom:'20px'}}>Gourmet Genius</h2>
                         <p>
-                        Gourmet Genius is a web application designed to streamline meal planning and enhance the culinary experience. The app allows users to discover and create personalized recipes, save their favorites, and organize them into custom meal plans. With a focus on simplicity and user-friendly design, Gourmet Genius integrates dynamic search functionality, intuitive navigation, and a seamless interface for users to explore new dishes and manage their culinary preferences. </p>
+                        Gourmet Genius is a web application designed to streamline meal planning and enhance the culinary experience. The app allows users to discover and create personalized recipes, save their favorites, and organize them into custom meal plans. </p>
                         <h3 style={{marginTop:"20px"}}>Click Here</h3>
                     </div>
                     </a>
@@ -35,7 +35,7 @@ return(
                    <img src={logo1} alt="this is an work1"/>
                     <div className="layer">
                         <h2 style={{marginBottom:'20px'}}>Food Classification Using CNN</h2>
-                        <p>Food Classification Using Convolutional Neural Networks (CNNs) is a machine learning technique that identifies and categorizes various food items based on images. This process starts with gathering a substantial dataset, such as the Food-101 dataset, which features over 101,000 images across 101 food categories. Data preprocessing is critical and includes resizing images, normalizing pixel values, and employing data augmentation techniques to enhance the training set. The CNN architecture consists of convolutional layers for feature extraction and pooling layers to reduce dimensionality.</p>
+                        <p>Food Classification Using Convolutional Neural Networks (CNNs) is a machine learning technique that identifies and categorizes various food items based on images. This process starts with gathering a substantial dataset, such as the Food-101 dataset, which features over 101,000 images across 101 food categories.</p>
                         <h3 style={{marginTop:"20px"}}>Click Here</h3>
                     </div>
                     </a>
